8b4f2400ac4c53f71d28e4c2e0fbceaf89b03eec
anastasiiasheben-creator <anastasiia.sheben@gmail.com>
2026-05-11 12:50:16 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { FaWhatsapp, FaFacebookF } from "react-icons/fa";
-import logoImg from './assets/logo.png';
+import logoImg from './assets/logo.webp';
 
 const API = 'https://webklienti-backend.onrender.com';
 
